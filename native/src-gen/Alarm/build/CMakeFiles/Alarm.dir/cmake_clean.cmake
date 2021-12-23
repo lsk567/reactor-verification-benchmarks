b8e4e6caeb3f052b0f131b7e356d516505be543f
Shaokai Lin <shaokai_lin@126.com>
2021-12-22 20:22:50 -0500

    Add the alarm pattern

---
+++ native/src-gen/Alarm/build/CMakeFiles/Alarm.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Alarm"
+  "Alarm.pdb"
+  "CMakeFiles/Alarm.dir/Alarm.c.o"
+  "CMakeFiles/Alarm.dir/core/platform/lf_macos_support.c.o"
+  "CMakeFiles/Alarm.dir/ctarget.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Alarm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
