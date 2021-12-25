55b33f7e25d59c396a816e9395689e440214196d
Shaokai Lin <shaokai_lin@126.com>
2021-12-25 17:03:04 -0500

    Start porting soter benchmarks

---
+++ soter/safe_send/src-gen/safe_send/build/CMakeFiles/safe_send.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/safe_send.dir/core/platform/lf_macos_support.c.o"
+  "CMakeFiles/safe_send.dir/ctarget.c.o"
+  "CMakeFiles/safe_send.dir/safe_send.c.o"
+  "safe_send"
+  "safe_send.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/safe_send.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ soter/unsafe_send/src-gen/unsafe_send/build/CMakeFiles/unsafe_send.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/unsafe_send.dir/core/platform/lf_macos_support.c.o"
+  "CMakeFiles/unsafe_send.dir/ctarget.c.o"
+  "CMakeFiles/unsafe_send.dir/unsafe_send.c.o"
+  "unsafe_send"
+  "unsafe_send.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/unsafe_send.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
