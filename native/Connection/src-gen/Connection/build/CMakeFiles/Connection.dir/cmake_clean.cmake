8264c1122728c407083dadc26a2f23da46511e96
Shaokai Lin <shaokai_lin@126.com>
2021-12-27 23:53:08 -0500

    Add the connection example

---
+++ native/Connection/src-gen/Connection/build/CMakeFiles/Connection.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Connection.dir/Connection.c.o"
+  "CMakeFiles/Connection.dir/core/platform/lf_macos_support.c.o"
+  "CMakeFiles/Connection.dir/ctarget.c.o"
+  "Connection"
+  "Connection.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Connection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
